daf1095767b965ef1cabd6662f01c237f849cc86
Landon Kihe <lkihe@hawaii.edu>
2025-02-05 20:58:17 -1000

    part 1 done

---
+++ sumofnumbers.js
@@ -0,0 +1,34 @@
+"use strict";
+function sumFor(array) {
+    let output = 0;
+    for (let x = 0; x < array.length; x++) {
+        output += array[x];
+    }
+    return output;
+}
+function sumWhile(array) {
+    let output = 0;
+    let x = 0;
+    while (x < array.length) {
+        output += array[x];
+        x++;
+    }
+    return output;
+}
+function sumRecursion(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    return array[0] + sumRecursion(array.slice(1));
+}
+function sumTheFunctionalWay(array) {
+    let output = 0;
+    array.forEach((item) => {
+        output += item;
+    });
+    return output;
+}
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
